ffcfa2008f422a961b71e511e76338e136bdea7e
Zabashta_Artur <zarpost@gmail.com>
2022-11-07 00:26:08 +0300

    feat: add solve of task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+//const copyFile = require('fs/promises');
+const srcDirPath = path.join(__dirname, 'files');
+const distDirPath = path.join(__dirname, 'files-copy');
 
+  fs.mkdir(distDirPath, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+
+    fs.readdir(srcDirPath, { withFileTypes: true }, (err, srcData) => {
+      if (err) {
+        console.log(err.message);
+      }
+          
+      srcData.forEach((file) => {
+        if (file.isFile()) {
+          
+          fs.copyFile(
+            srcDirPath + '/' + file.name,
+            distDirPath + '/' + file.name,
+            (err) => {
+              if (err) {
+                console.log(err.message);
+              }
+            }
+          );
+        }
+      });
+
+      fs.readdir(distDirPath, { withFileTypes: true }, (err, distData) => {
+        if (err) {
+          console.log(err.message);
+        }        
+
+        distData.forEach((distFile) => {
+          let isFileNeed = false;
+
+          srcData.forEach((srcFile) => {
+            if (srcFile.name === distFile.name)  isFileNeed = true;
+          })
+
+          if (isFileNeed === false) {
+            fs.unlink(distDirPath + '/' + distFile.name, function () {});
+          }          
+          
+        })  
+      })
+    })
+  }
+})
+
+
+
+  
